2c2fbaeac68ca778c0bf5fa441aaacfbfcea37c4
albinagrytsiv <99601540+albinagrytsiv@users.noreply.github.com>
2022-02-23 01:51:07 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("Test homework")
+const CarrotPrice=15.678;
+const BroccoliniPrice=123.965;
+const PapayaPrice=90.2345;
+
+const maxPrice=Math.max(CarrotPrice,BroccoliniPrice,PapayaPrice);
+console.log(maxPrice);
